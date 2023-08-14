50e5d5dab47cd5056d2d11ffe4842dbcd2079101
simon <18758095949@163.com>
2023-08-14 13:57:34 +0800

    进行过回测实验的代码

--- 平台回测demo/new_backtest/ENGINIE.py
+++ 平台回测demo/new_backtest/ENGINIE.py
@@ -1,40 +1,42 @@
 import threading
 import pandas as pd
 import numpy as np
-from calculate_position import *
+from tqdm import trange
+from new_backtest.calculate_position import *
+
 
 # 二维pivot数据以时间为columns， 以股票为index.
 
-class DATA:
+class DATA(object):
     _instance_lock = threading.Lock()
-    
+    known_data = {}
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(DATA, "_instance"):
             with DATA._instance_lock:
                 if not hasattr(DATA, "_instance"):
-                    DATA._instance = object.__new__(cls, *args, **kwargs)
+                    DATA._instance = object.__new__(cls)
         return DATA._instance
-    
+
     # 传入的source数据必须要结构化，columns日期"trddt"；代码为"code"。
     def __init__(self, source):
 
         if not hasattr(self, '_inited'):
-            self.known_data = {'close': None, 'preclose': None, 'open': None, 'pctchange': None, 'vol': None, 'amt': None, "udstatus": None,
-                        'tradestatus': None, 'avg': None}
+            self._inited = True
+            self.known_data = {'close': None, 'preclose': None, 'open': None, 'pctchange': None, 'vol': None,
+                               'amt': None, "udstatus": None,
+                               'tradestatus': None, 'avg': None}
             for data_name in self.known_data:
                 pivot_table = source.pivot(index="code", columns="trddt", values=data_name)
                 self.known_data[data_name] = pivot_table
-            self._inited = True
-        
-        
+
     # 为了避免定义属性环节过于冗杂，故使用__getattr__方法。
-    def __getattribute__(self, __name):
-        if __name in self.known_data:
-            return self.known_data[__name]
+    def __getattr__(self, attr_name):
+        if attr_name in self.known_data:
+            return self.known_data[attr_name]
         else:
-            raise AttributeError(f"Attribute '{__name}' for AshareData not defined.")
-        
+            raise AttributeError("Attribute '{}' for AshareData not defined.".format(attr_name))
+
     # 获取打分数据，在这之前要将分数数据结构化
     def get_score_data(self, score):
         # 首先获取数据框架frame，以让其与其他价格数据等对应
@@ -49,9 +51,9 @@ class BACK_TEST:
         if not hasattr(BACK_TEST, "_instance"):
             with BACK_TEST._instance_lock:
                 if not hasattr(BACK_TEST, "_instance"):
-                    BACK_TEST._instance = object.__new__(cls, *args, **kwargs)
+                    BACK_TEST._instance = object.__new__(cls)
         return BACK_TEST._instance
-    
+
     # 初始化从DATA类中获取数据，作为我进行回测的资源，这是获取原材料
     # 然后再进一步构建工程线，去加工获得的原材料，即BACK_TEST类里面的method(方法)
     def __init__(self, data_object):
@@ -61,23 +63,23 @@ class BACK_TEST:
             # 初始化账户数据等，作为计量策略表现的依据
             self.TOTAL_position = pd.DataFrame(np.zeros(self.data.close.shape),
                                                index=self.data.close.index, columns=self.data.close.columns)
-            self.position_DAILY_return = pd.Series(data=np.zeros(len(self.data.close.columns)), index=self.data.close.columns)
+            self.position_DAILY_return = pd.Series(data=np.zeros(len(self.data.close.columns)),
+                                                   index=self.data.close.columns)
             self._inited = True
+            self.net_value_change = None
 
+    def start_test(self, number_of_stock=100, weight_of_one_stock=0.01, principle=1000000):  # 开始回测的过程
+        length = len(self.data.close.columns)
 
-
-    def start_test(self, number_of_stock, weight_of_one_stock=0.01):# 开始回测的过程
-        next_postition =
-
-        for date in self.data.close.columns:
-            calculate_table = pd.cancat([self.TOTAL_position.loc[:,date], self.data.score_frame.loc[:,date]],axis=1)
+        for i in trange(1, length):
+            calculate_table = pd.concat([self.TOTAL_position.loc[:, self.data.close.columns[i - 1]],
+                                         self.data.score_frame.loc[:, self.data.close.columns[i - 1]]], axis=1)
+            # calculate_table.to_csv("calculate_table_{}.csv".format(i))
             calculate_table.columns = ["present_position", "score"]
-            next_postition = cal_new_position(calculate_table)
+            new_position = cal_new_position(calculate_table)  # 根据score进行排名并分配权重
+            # new_position.to_csv("new_position_{}.csv".format(i))
+            self.TOTAL_position.loc[:, self.data.close.columns[i]].update(new_position)  # 对TOTAL_position进行更新
 
-
-
-
-
-        
-        
-
+        stock_position_change = (self.data.pctchange / 100) * self.TOTAL_position  # 计算每只股票贡献的增长率
+        totalposition_change = stock_position_change.sum().T  # 将所有股票贡献的成长率加总，获得position的增长率
+        self.net_value_change = (1 + totalposition_change).cumprod() * principle
---
+++ 平台回测demo/new_backtest/main.py
@@ -0,0 +1,25 @@
+from new_backtest.ENGINIE import DATA
+from new_backtest.ENGINIE import BACK_TEST
+import pandas as pd
+import sqlalchemy
+import datetime
+
+# get data from read_only MYSQL
+CONN_RESEARCH = sqlalchemy.create_engine('mysql+pymysql://readonly:Only4$Reading@home.jinxiong.work:53306/research')
+d1 = datetime.date(2023, 1, 1)
+d2 = datetime.date(2023, 8, 1)
+query = "SELECT * from quote_ashareeod_wind where trddt>='{}' and trddt<='{}'".format(d1, d2)
+data_sql = pd.read_sql_query(query, CONN_RESEARCH)
+data = data_sql[
+    ['trddt', 'windcode', 'clsprc', 'preclose', 'opprc', 'pctchange', 'vol', 'amt', "prclmtsts", 'tradests', 'avgprc']]
+data.columns = ['trddt', 'code', 'close', 'preclose', 'open', 'pctchange', 'vol', 'amt', "udstatus", 'tradestatus',
+                'avg']
+
+DATA_OBJ = DATA(data)
+DATA_OBJ.get_score_data(DATA_OBJ.pctchange)
+
+INS_TEST = BACK_TEST(DATA_OBJ)
+INS_TEST.start_test()
+
+INS_TEST.net_value_change.to_csv("net_value_change.csv")
+print(INS_TEST.net_value_change)
